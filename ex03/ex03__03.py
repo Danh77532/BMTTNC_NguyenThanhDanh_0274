efaf4c4d25e22c464f38c750ea8103931247d656
Danh77532 <danhnguyen021204@gmail.com>
2026-02-02 16:24:56 +0700

    [add] lab01 ex03

---
+++ ex03/ex03__03.py
@@ -0,0 +1,7 @@
+def tao_tuble_tu_list(lst):
+    return tuple(lst)
+input_list = input( input("Nhập danh sách các số, cách nhau bằng dấu phẩy"))
+numbers = list(map(int, input_list.split(',')))
+my_tuple = tao_tuble_tu_list(numbers)
+print("List: ",numbers)
+print("Tuple từ List:",my_tuple)
